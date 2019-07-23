ef586b6adc792593428ca9ab07301c7087f1b0eb
= <=>
2019-07-23 22:57:37 +0800

    配置生产环境的ip

---
+++ src/main/java/com/zhu/itoken/common/domain/Posts.java
@@ -0,0 +1,45 @@
+package com.zhu.itoken.common.domain;
+
+import java.util.Date;
+
+public class Posts {
+    private Integer id;
+
+    private String name;
+
+    private Date createTime;
+
+    private Date updataTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdataTime() {
+        return updataTime;
+    }
+
+    public void setUpdataTime(Date updataTime) {
+        this.updataTime = updataTime;
+    }
+}
